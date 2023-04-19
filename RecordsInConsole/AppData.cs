a72f0559fce570d971ebd0835fcb9844e012cee3
Evgenii_Shishkunov <evgenii.shishkunov@gmail.com>
2023-04-19 18:20:05 +0700

    Create Record.cs and AppData.cs

---
+++ RecordsInConsole/AppData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordsInConsole
+{
+	internal class AppData
+	{
+		public List<Record> records { get; private set; }
+
+		private int _assignId;
+
+		public AppData()
+		{
+			records = new List<Record>();
+			_assignId = 0;
+		}
+
+		public void AddRecord(Record record)
+		{
+			if (records == null)
+				return;
+
+			record.id = _assignId++;
+			records.Add(record);
+		}
+
+		public void DeleteRecord(int id)
+		{
+			Record removingRecord = records.Find(r => r.id == id);
+			if (removingRecord != null)
+				records.Remove(removingRecord);
+		}
+	}
+}
