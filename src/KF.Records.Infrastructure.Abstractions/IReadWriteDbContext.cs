45c10ac0b2be6b8771c57c50bb32a73bfe73108d
Evgenii_Shishkunov <evgenii.shishkunov@gmail.com>
2023-06-21 17:13:06 +0700

    Add entity framework via nuget, add IReadWriteDbContext.cs

---
+++ src/KF.Records.Infrastructure.Abstractions/IReadWriteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using KF.Records.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KF.Records.Infrastructure.Abstractions;
+internal interface IReadWriteDbContext
+{
+    public DbSet<Record> records { get; set; }
+}
