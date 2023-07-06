fb9afc796cfd950bf0590b9fde7ea1fdca79ade8
Evgenii_Shishkunov <evgenii.shishkunov@gmail.com>
2023-07-06 14:47:27 +0700

    Add summary, minor fixes

--- src/KF.Records.UseCases/Records/AddRecord/AddTagDto.cs
+++ src/KF.Records.UseCases/Records/AddRecord/AddTagDto.cs
@@ -5,7 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace KF.Records.UseCases.Records.AddRecord;
+
+/// <summary>
+/// Tag dto
+/// </summary>
 public class AddTagDto
 {
+    /// <summary>
+    /// Tag name
+    /// </summary>
     required public string Name { get; set; }
 }
